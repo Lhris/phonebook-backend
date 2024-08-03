283f1c5e8495d2f8cc0ae56b2f45b2dbb4f6ffd6
Chris Lee <chrissept2@gmail.com>
2024-08-02 21:09:33 -0700

    3.17 - complete

--- index.js
+++ index.js
@@ -56,6 +56,20 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person).then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 

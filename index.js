00975705911a5b20ff83795b6d7c4d71827fcc0d
Chris Lee <chrissept2@gmail.com>
2024-08-09 21:04:31 -0700

    3.19 - complete

--- index.js
+++ index.js
@@ -51,11 +51,14 @@ app.post('/api/persons', (request, response, next) => {
 
     person.save().then(savedPerson => {
       response.json(savedPerson)
-    })}
+    }).catch(error =>
+      next(error)
+    )}
   })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndDelete(request.params.id)
+  Person.findByIdAndDelete(
+    request.params.id)
   .then(result => {
     response.status(204).end()
   })
@@ -70,7 +73,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person).then(updatedPerson => {
+  Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true, context: 'query'}).then(updatedPerson => {
     response.json(updatedPerson)
   })
   .catch(error => next(error))
@@ -81,7 +84,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }  else if (error.name === 'ValidationError') {
+    return response.status(400).send({error: error.message})
+  }
 
   next(error)
 }
